e693d8effe648f17412d802dc6c1418d2a655d36
sherlock <sherlotris@outlook.com>
2020-02-23 19:38:46 +0100

    use passport to authenticate login process, and add navbar

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const port = 3000
 
 
@@ -14,11 +15,15 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({ secret: 'iloveming', resave: false, saveUninitialized: false }))
-app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash("success_messages")
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
 
@@ -27,4 +32,4 @@ app.listen(port, () => {
   console.log(`Example app is on port${port}!`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
