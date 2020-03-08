865c3548ef328ef5e89ebba463684164e2df17c5
sherlock <sherlotris@outlook.com>
2020-03-08 23:09:47 +0100

    finish classification and pagination for the front-end

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,9 @@ module.exports = (app, passport) => {
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
   /*===================================*/
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
 
+  /*===================================*/
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
