eb831d1686134b63ff136e702e9784d394783e1f
sherlock <sherlotris@outlook.com>
2020-03-08 13:42:43 +0100

    change port location for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
